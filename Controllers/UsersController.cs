ba9783e386c0468d14100a68d7cca61b334f9dfe
Krystof Senfeld <ksenfeld@hotmail.com>
2025-05-09 18:05:49 +0100

    Created barebones pages and functionality for user search.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using GitHubSearch.Models.Users;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,6 +9,19 @@ using System.Web.Mvc;
 namespace GitHubSearch.Controllers {
     public class UsersController : Controller {
         public ActionResult Index() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(UserSearchModel model) {
+            if (!ModelState.IsValid) {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("UserSearchResult");
+        }
+
+        public ActionResult UserSearchResult() {
             return View();
         }
     }
---
+++ Models/Users/UserSearchModel.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GitHubSearch.Models.Users {
+    public class UserSearchModel {
+        [Required(ErrorMessage = "Please provide a username.")]
+        public string Username { get; set; }
+    }
+}

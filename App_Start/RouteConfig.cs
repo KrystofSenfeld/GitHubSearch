76178af511c4c50ecb63ff0c04b8ae14983cf063
Krystof Senfeld <ksenfeld@hotmail.com>
2025-05-09 16:24:39 +0100

    Removed unneeded content and replaced placeholder titles

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace GitHubSearch {
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubSearch.Controllers {
+    public class UsersController : Controller {
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}

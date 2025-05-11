1254ed72a01092250bf68afa56f706228e8fe0f7
Krystof Senfeld <ksenfeld@hotmail.com>
2025-05-11 16:52:28 +0100

    Created GitHubClient.

---
+++ Services/GitHubClient.cs
@@ -0,0 +1,50 @@
+﻿using GitHubSearch.Models.JSON;
+using GitHubSearch.Models.JSON.Repos;
+using GitHubSearch.Models.JSON.Users;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace GitHubSearch.Services {
+    public class GitHubClient : HttpClient {
+        string apiBaseUrl = "https://api.github.com/";
+
+        public GitHubClient() {
+            BaseAddress = new Uri(apiBaseUrl);
+            DefaultRequestHeaders.Clear();
+            DefaultRequestHeaders.Add("User-Agent", "GitHub Search");
+            DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
+        }
+
+        public async Task<ApiResponse<T>> GetAsync<T>(string url, bool isUrlAbsolute = false) {
+            if (!isUrlAbsolute) {
+                url = apiBaseUrl + url;
+            }
+
+            var response = await base.GetAsync(url);
+            var responseModel = new ApiResponse<T>();
+
+            responseModel.StatusCode = response.StatusCode;
+            responseModel.WasSuccessful = response.IsSuccessStatusCode;
+            responseModel.Message = response.ReasonPhrase;
+
+            if (response.IsSuccessStatusCode) {
+                var data = await response.Content.ReadAsStringAsync();
+                responseModel.Result = JsonConvert.DeserializeObject<T>(data);
+            }
+
+            return responseModel;
+        }
+
+        public async Task<ApiResponse<UserResponseModel>> GetUserByUsername(string username) {
+            return await GetAsync<UserResponseModel>($"users/{username}");
+        }
+
+        public async Task<ApiResponse<List<RepoResponseModel>>> GetUserReposByAbsoluteUrl(string url) {
+            return await GetAsync<List<RepoResponseModel>>(url, true);
+        }
+    }
+}

a5e8a0cb824eeb0d3d601fb86fcfb0f5a09e8d70
Krystof Senfeld <ksenfeld@hotmail.com>
2025-05-11 16:53:38 +0100

    Fleshed out controller logic.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,28 +1,75 @@
-﻿using GitHubSearch.Models.Users;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using GitHubSearch.Models.DTO;
+using GitHubSearch.Models.Users;
+using GitHubSearch.Services;
+using GitHubSearch.Utility;
 using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace GitHubSearch.Controllers {
     public class UsersController : Controller {
-        public ActionResult Index() {
-            return View();
+        [LoadTempModelState]
+        public ActionResult Index(string searchError) {
+            var viewModel = new IndexViewModel {
+                Title = "User Search",
+                Error = searchError,
+                UserSearch = new UserSearchDto(),
+            };
+
+            return View(viewModel);
         }
 
         [HttpPost]
-        public ActionResult Search(UserSearchModel model) {
+        [SaveTempModelState]
+        public ActionResult Search(UserSearchDto model) {
             if (!ModelState.IsValid) {
                 return RedirectToAction("Index");
             }
 
-            return RedirectToAction("UserSearchResult");
+            return RedirectToAction("UserSearchResult", model);
         }
 
-        public ActionResult UserSearchResult() {
-            return View();
+        public async Task<ActionResult> UserSearchResult(UserSearchDto model) {
+            if (model == null || string.IsNullOrEmpty(model.Username)) {
+                return RedirectToAction("Index");
+            }
+
+            var viewModel = new UserSearchResultViewModel();
+
+            using (var client = new GitHubClient()) {
+                var user = new UserDto();
+                var userResponse = await client.GetUserByUsername(model.Username);
+
+                if (!userResponse.WasSuccessful) {
+                    return RedirectToAction("Index", "Users",
+                        new { searchError = $"The user '{model.Username}' could not be found." });
+                }
+
+                user.Username = userResponse.Result.Username;
+                user.Url = userResponse.Result.Url;
+                user.AvatarUrl = userResponse.Result.AvatarUrl;
+                user.Location = userResponse.Result.Location;
+
+                var reposResponse = await client.GetUserReposByAbsoluteUrl(userResponse.Result.ReposUrl);
+
+                if (reposResponse.WasSuccessful) {
+                    user.Repos = reposResponse.Result
+                        .OrderByDescending(r => r.StargazersCount)
+                        .Take(5)
+                        .Select(r => new UserRepoDto {
+                            Name = r.Name,
+                            Url = r.Url,
+                            Description = r.Description,
+                            StargazersCount = r.StargazersCount,
+                        })
+                        .ToList();
+                }
+
+                viewModel.User = user;
+                viewModel.Title = $"User Search: {model.Username}";
+            }
+
+            return View(viewModel);
         }
     }
 }
